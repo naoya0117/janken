26882f024a233668f981861fdd7951e0cfac7104
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-10-04 01:43:09 +0900

    ユーザ入力とjanken.htmlへの遷移

--- src/main/java/oit/is/z1992/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1992/kaizi/janken/controller/JankenController.java
@@ -1,3 +1,15 @@
 package oit.is.z1992.kaizi.janken.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class JankenController {
+    @PostMapping("/janken")
+    public String janken(ModelMap model, @RequestParam String username) {
+        model.addAttribute("username", username);
+        return "janken.html";
+    }
 }
