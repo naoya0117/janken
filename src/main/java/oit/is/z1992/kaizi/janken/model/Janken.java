b974beb2ee4a84f59c410b7a0c092d8bf61156e9
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-10-05 23:41:30 +0900

    じゃんけんゲームの実装

---
+++ src/main/java/oit/is/z1992/kaizi/janken/model/Janken.java
@@ -0,0 +1,51 @@
+package oit.is.z1992.kaizi.janken.model;
+
+import java.util.Random;
+
+public class Janken {
+    private String cpu;
+    private String player;
+
+    public Janken(String player) {
+        this.player = player;
+        int rand = new Random().nextInt(3);
+
+        switch (rand) {
+            case 0:
+                this.cpu = "Gu";
+                break;
+            case 1:
+                this.cpu = "Choki";
+                break;
+            case 2:
+                this.cpu = "Pa";
+                break;
+        }
+    }
+
+    public String getResult () {
+        if (this.cpu.equals(this.player)) {
+            return "Draw";
+        }
+        if (this.cpu.equals("Gu") && this.player.equals("Choki") || 
+            this.cpu.equals("Choki") && this.player.equals("Pa") ||
+            this.cpu.equals("Pa") && this.player.equals("Gu")) {
+
+            return "You Lose";
+        } 
+        if (this.cpu.equals("Gu") && this.player.equals("Pa") || 
+            this.cpu.equals("Choki") && this.player.equals("Gu") ||
+            this.cpu.equals("Pa") && this.player.equals("Choki")) {
+
+            return "You Win!";
+        }
+        return "Error";
+    }
+
+    public String getCpu() {
+        return this.cpu;
+    }
+    public String getPlayer() {
+        return this.player;
+    }
+}
